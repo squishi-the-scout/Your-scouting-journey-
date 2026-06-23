6783ab24bab7da5913528c518d24e9b59d7cfafc
squishi-the-scout <squishi.scout@gmail.com>
2026-06-24 02:24:44 +0800

    Create secondclass-requirements.js

---
+++ data/data/secondclass-requirements.js
@@ -0,0 +1,18 @@
+export const secondClassRequirements = [
+  { id: 1, name: "Scouting History" },
+  { id: 2, name: "Pitch Strike and Store a Hike or Patrol Tent" },
+  { id: 3, name: "Knots and Lashing" },
+  { id: 4, name: "Wood Craft Signs" },
+  { id: 5, name: "Hand Axe, Froe and Kathi Valhi" },
+  { id: 6, name: "Cooking" },
+  { id: 7, name: "Fire Lighting" },
+  { id: 8, name: "Hike" },
+  { id: 9, name: "First Aid" },
+  { id: 10, name: "Rules of Health" },
+  { id: 11, name: "Swimming" },
+  { id: 12, name: "Observation Skills" },
+  { id: 13, name: "Common Trees, Birds and Fishes" },
+  { id: 14, name: "Compass and the Safety Regulations of a Sea Going Vessel" },
+  { id: 15, name: "Environmental Education" },
+  { id: 16, name: "Re-test Scout Standard" }
+];
