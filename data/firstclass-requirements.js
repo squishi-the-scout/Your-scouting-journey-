605d284247b894de7e605672cc988ba00d2dec57
squishi-the-scout <squishi.scout@gmail.com>
2026-06-24 02:30:42 +0800

    Create firstclass-requirements.js

---
+++ data/firstclass-requirements.js
@@ -0,0 +1,16 @@
+export const firstClassRequirements = [
+  { id: 1, name: "Emergencies" },
+  { id: 2, name: "First Aid" },
+  { id: 3, name: "Common Trees, Birds and Fishes" },
+  { id: 4, name: "Felling Axes and Maldivian Tools Mulhoa, Odaa" },
+  { id: 5, name: "Mapping and Compass" },
+  { id: 6, name: "Estimation" },
+  { id: 7, name: "Knots, Lashing and Splices" },
+  { id: 8, name: "Tracking" },
+  { id: 9, name: "Swimming" },
+  { id: 10, name: "Cooking" },
+  { id: 11, name: "Camping" },
+  { id: 12, name: "Environmental Education" },
+  { id: 13, name: "Hike - Expedition" },
+  { id: 14, name: "Re-test Advance Scout Standard" }
+];
